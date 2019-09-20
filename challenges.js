3b01f4be87cff4a2ea981d9acc42bc83989af7b0
iheartobi <iobidah9@gmail.com>
2019-09-19 22:13:57 -0400

    added second die and user input for score

--- challenges.js
+++ challenges.js
@@ -1,41 +1,35 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-
 let scores, roundScore, activePlayer, gamePlaying;
+let lastDice;
 init() 
+
 
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
     if (gamePlaying){
 
             //button generates a random number
-let dice = Math.floor(Math.random() * 6) + 1
+let dice1 = Math.floor(Math.random() * 6) + 1
+let dice2 = Math.floor(Math.random() * 6) + 1
+
 
 //display result
-let diceDOM = document.querySelector('.dice')
-diceDOM.style.display = 'block'
-diceDOM.src = 'dice-' + dice + '.png'
+document.getElementById('dice-1').style.display = 'block'
+document.getElementById('dice-2').style.display = 'block'
+document.getElementById('dice-1').src="dice-" + dice1 + '.png'
+document.getElementById('dice-2').src="dice-" + dice2 + '.png'
+
 
 //update round score only if number isn't 1
-//if player rolls 2 sixes in a row global score is equal to 0
-if (dice !== 1) {
+
+if (dice1 !== 1 && dice2 !== 1) {
     //add score
-    roundScore += dice
+    roundScore += dice1 + dice2
     document.querySelector('#current-' + activePlayer).textContent = roundScore
 
 }else {
     //next player
     nextPlayer()
 }
-
     }
 
 } )
@@ -47,11 +41,20 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 
         //update the user interface
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer] 
+
+        let input = document.querySelector('.final-score').value
+        let winningScore
+        if(input){
+            winningScore = input
+        }else{
+            winningScore = 100
+        }
     
         //check if player won game
-        if (scores[activePlayer] >= 20){
+        if (scores[activePlayer] >= winningScore){
             document.querySelector('#name-' + activePlayer).textContent = 'WINNER!!'
-            document.querySelector('.dice').style.display = 'none'
+            document.getElementById('dice-1').style.display = 'none'
+            document.getElementById('dice-2').style.display = 'none'
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner') 
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active') 
             gamePlaying = false
@@ -73,7 +76,8 @@ function nextPlayer(){
     document.querySelector('.player-0-panel').classList.toggle('active')
     document.querySelector('.player-1-panel').classList.toggle('active')
 
-    document.querySelector('.dice').style.display = 'none'
+    document.getElementById('dice-1').style.display = 'none'
+    document.getElementById('dice-2').style.display = 'none'
 }
 
 document.querySelector('.btn-new').addEventListener('click', init)
@@ -89,7 +93,10 @@ function init(){
     let player2 = prompt('Who is Player 2 ?')
 
     
-    document.querySelector('.dice').style.display = 'none'
+    document.querySelector('#dice-1').style.display = 'none'
+    document.querySelector('#dice-2').style.display = 'none'
+
+    document.querySelector('.final-score').textContent = 'Final Score'
 
     document.getElementById('score-0').textContent = '0'
     document.getElementById('score-1').textContent = '0'
@@ -105,10 +112,4 @@ function init(){
 
 
 }
-
-
-
-
-
-
 
